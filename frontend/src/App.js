f4dfae7180dde9b9bcc8b9a63608dd62d7f57d27
Karan Sharma <sharma.karan27oct@gmail.com>
2022-10-19 18:15:05 +0530

    added actions and reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Posts from './components/Posts/Posts.js'
+import Form from './components/Form/Form.js'
 import memories from './images/memories.png'
-import {AppBar, Container,Grid,Grow,Typography} from '@material-ui/core'
-import Form from './components/Form/Form'
-import Posts from './components/Posts/Posts'
-
+import { useDispatch } from 'react-redux'
+import {getPosts} from './actions/posts'
+import './App.css'
 const App = () => {
+  const dispatch=useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch])
+  
   return (
-    <Container maxWidth="lg">
-      <AppBar position='static' color='inherit'>
-        <Typography variant='h2' align='center'>Memories</Typography>
-        <img src={memories} alt="" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justify='space-between' alignItems='stretch' spacing={60}>
-            <Grid item xs={12} sm={7}>
-              <Posts/>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form/>
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <>
+      <div className="upper">
+        <div className="memoriesContainer">
+          <h3 className='introTitle'>Memories</h3>
+          <img src={memories} alt="" />
+        </div>
+      </div>
+      <div className="lower">
+        <div className="postsContainer">
+          <Posts/>
+        </div>
+        <div className="formContainer">
+          <Form/>
+        </div>
+      </div>
+    </>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore,applyMiddleware,compose } from 'redux';
+import thunk from 'react-thunk'
+import reducers from './reducers'
 import App from './App';
 
+const store=createStore(reducers,compose(applyMiddleware(thunk)));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
